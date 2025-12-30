6905c57d3211a98155b03ecb67938a4554a142e4
suraj <amrutkarsuraj98@gmail.com>
2025-12-31 02:07:18 +0530

    Fix manifestPlaceholders syntax for Maps API key

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,19 +20,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.farmsetu_assignment"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 
-        manifestPlaceholders = [
-            MAPS_API_KEY: project.properties["MAPS_API_KEY"] ?: ""
-        ]
+        manifestPlaceholders["MAPS_API_KEY"] =
+            project.findProperty("MAPS_API_KEY") as String? ?: ""
     }
+
 
     buildTypes {
         release {
